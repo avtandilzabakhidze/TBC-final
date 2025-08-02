8cf29e1266f059ecc623957a57e0c635fab0ac6f
tramishivli <tornikeramishvili5@gmail.com>
2025-08-02 20:21:48 +0400

    Allure baby

--- src/test/java/BlogScrollSearchTest.java
+++ src/test/java/BlogScrollSearchTest.java
@@ -3,14 +3,21 @@ import ge.tbcacademy.data.model.blogs.BlogResponse;
 import ge.tbcacademy.enums.BlogSegment;
 import ge.tbcacademy.enums.Locale;
 import ge.tbcacademy.steps.BlogSteps;
+import io.qameta.allure.*;
+import io.qameta.allure.testng.AllureTestNg;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static ge.tbcacademy.data.Constants.PAGE_SIZE;
 import static ge.tbcacademy.data.Constants.SEARCH_TITLE;
 
-
+@Epic("API")
+@Link(name = "CRM-T12", url = "https://shorturl.at/bGVVf")
+@Listeners(AllureTestNg.class)
 public class BlogScrollSearchTest extends BaseTest {
-    @Test
+    @Story("Test Currency Exchange Rate Public API")
+    @Test(description = "Fetch Blogs Until Title Is Found or Confirmed Missing")
+    @Severity(SeverityLevel.CRITICAL)
     public void scrollSearchUsingPojoAndSteps() {
         int pageIndex = 0;
         boolean titleFound = false;
